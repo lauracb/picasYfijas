ba81e5af86e85f94aeaea3b033d204180d346c2f
lauracb <lauracadavidb@gmail.com>
2019-03-14 23:21:17 -0500

    Funcionalidad número aleatorio

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+var numberRamdon = Math.round(Math.random() * (10000 - 1000) + 1000); //falta incluir que el número sea diferentes dígitos.
+numberRamdon = numberRamdon.toString() //Convierte a string el número
 $('#number').keypress(function(event){
 	var numberUser = $(this).val();
+	var exp = /^[0-9]+(?:\[0-9]+)?$/; // expresión para validar número entero
 	var key = event.which;
-	if(key == 13){  	
-		alert(numberUser)
-  }   
+		if(key == 13){  	
+		event.preventDefault();
+		if((exp.test(numberUser)) && (numberUser.length === 4)){ //Si es número y si es de 4 cifras // falta validar que los dígitos no sean repetidos
+
+
+			// logica
+			numberRamdonArray = numberRamdon.split("");
+			numberUserArray = numberUser.split("");
+		
+
+
+				$('tbody').append('<tr><td>'+ numberUser +'<td></td><td></td></td></tr>')
+				$('span').removeClass('invalid');
+				$('input').removeClass('invalid-input');
+				$(this).val('');			
+		} else {
+			$('span').addClass('invalid');
+			$('input').addClass('invalid-input');
+			$(this).val('');
+		};
+	}   
 });
+
+
+  
